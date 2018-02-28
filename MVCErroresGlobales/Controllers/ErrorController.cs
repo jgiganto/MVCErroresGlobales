8226362435a42fcdf485947f24397ed313675f7d
jgiganto <jgiganto1@hotmail.com>
2018-02-28 13:30:20 +0100

    g

---
+++ MVCErroresGlobales/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+ 
+
+namespace MVCErroresGlobales.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //Este controlador solo sera para la parte grafica , 
+        //donde llevaremos las peticiones en caso de error
+        // GET: Error
+        public ActionResult PaginaNoEncontrada()
+        {
+            //Cuando tenemos Errores HTTP (httpexception)
+            //debemos indicar la respuesta del estado al servidor
+            //la clase HttpStatusCode contiene los codigos de error para las respuestas al servidor
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            ViewBag.Mensaje = "Pagina no encontrada en el servidor";
+            return View();
+        }
+        public ActionResult ErrorGeneral()
+        {
+            ViewBag.Mensaje = "Error general en la app";
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MVCErroresGlobales/Global.asax.cs
+++ MVCErroresGlobales/Global.asax.cs
@@ -17,5 +17,32 @@ namespace MVCErroresGlobales
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        //EN ESTE METODO SE CAPTURAN LAS PETICIONES DE ERROR
+        //INCONTROLABLES POR FILTER O POR EXCEPTION
+        protected void Application_Error()
+        {
+            //debemos crear la excepcion que esta llamando al metodo actualmente
+            Exception ex = Server.GetLastError();
+            //estamos en entorno http las excepciones que queremos capturar tienen codigos http
+            //debemos convertir nuestro exception general a un tipo de excepcion http
+            HttpException httpexception = ex as HttpException;
+            //el objeto httpexception contiene los codigos de error http
+            //almacenamos la accion donde deseamos enviar dependiendo de los errores http
+            String accion = "";
+            switch (httpexception.ErrorCode)
+            {
+                case 404:
+                    accion = "PaginaNoEncontrada";
+                    break;
+                case 403: //forbidden
+                    accion = "ErrorGeneral";
+                    break;
+                default:
+                    accion = "ErrorGeneral";
+                    break;
+
+            }
+        }
     }
 }
