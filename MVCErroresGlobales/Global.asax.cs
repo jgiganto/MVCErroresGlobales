ce491c5cf5486eda209cdda244f42df3fe93448f
jgiganto <jgiganto1@hotmail.com>
2018-02-28 13:54:45 +0100

    s

--- MVCErroresGlobales/Global.asax.cs
+++ MVCErroresGlobales/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCErroresGlobales.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -43,6 +44,23 @@ namespace MVCErroresGlobales
                     break;
 
             }
+            //al capturar las excepciones globales debemos limpiar el contexto del error
+            Context.ClearError();
+            //redireccionamos pero no lo vamos a hacer con routing
+            //vamos a enviar la peticion request directamente al controlador error
+            //necesitamos ROUTEdata para indicar donde vamos a ir
+            RouteData rutaerror = new RouteData();
+            //añadimos a la ruta el controlador y el action
+            rutaerror.Values.Add("controller", "Error");
+            rutaerror.Values.Add("action", accion);
+            //para poder ejecutar la peticion a un controlador 
+            //con una ruta, necesitamos la clase icontroller que permite ejecutar otra peticion
+            IController controlador = new ErrorController();
+            //mediante el metodo execute, redirigimos el request, enviando el contexto(Context) y RouteData
+            controlador.Execute(
+                new RequestContext(new HttpContextWrapper(Context), rutaerror));
+
+
         }
     }
 }
